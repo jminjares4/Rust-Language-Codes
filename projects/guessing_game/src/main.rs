241a09e9110a9a6508a75172f49fd7ea8f0872be
jminjares4 <jminjares4@miners.utep.edu>
2021-12-14 11:58:39 -0700

    Updated main.rs

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-
+    //trime -> remove any white spaces and \r\n,  
     let guess: u32 = guess.trim().parse().expect("Please type a number!");
     println!("You have guessed: {}", guess);
 

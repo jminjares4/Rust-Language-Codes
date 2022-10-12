2e0ca3fb8e3e030ca004c3f62fa783c4f6b8f0a7
jminjares4 <minjares.jesus.ths@gmail.com>
2022-10-12 13:26:02 -0600

    Chapter 3 first section codes

---
+++ Chapter 3/Variables and Mutability/mutable_variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let mut x = 5; // set x to be mutable and assign it 5
+    // display value of x 
+    println!("The value of x is: {x}");
+    x = 6; // update x 
+    // display value of x 
+    println!("The value of x is: {x}");
+}

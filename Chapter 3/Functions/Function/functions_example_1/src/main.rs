1e602dc62fe5ac02e55d24abed2f9d1eb4e77349
jminjares4 <minjares.jesus.ths@gmail.com>
2022-10-12 18:03:29 -0600

    Added Function section of chapter 3

---
+++ Chapter 3/Functions/Function/functions_example_1/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // default cargo new output
+    println!("Hello, world!");
+
+    another_function();
+}
+
+// create another function
+fn another_function(){
+    // display message
+    println!("Another function.");
+}
---
+++ Chapter 3/Functions/Functions with Return Values/return_value_example_1/src/main.rs
@@ -0,0 +1,11 @@
+fn five() -> i32 {
+    // return a 5 implicitly 
+    5
+}
+
+fn main(){
+    // call function call
+    let x = five();
+    // display message 
+    println!("The value of x is: {x}");
+}

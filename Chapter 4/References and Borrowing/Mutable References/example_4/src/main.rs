494facefb19fb053e8fc5f1f7969f7afe678ed6a
jminjares4 <minjares.jesus.ths@gmail.com>
2023-07-08 23:27:28 -0600

    Added References and Borrowing

---
+++ Chapter 4/References and Borrowing/Mutable References/example_4/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; // BIG PROBLEM
+
+    println!("{}, {}, {}", r1, r2, r3);
+}

445a6a0abeb7d4069961532e078db8d4f11bb30b
jminjares4 <minjares.jesus.ths@gmail.com>
2023-07-10 19:24:03 -0600

    Added The Slicing Type

---
+++ Chapter 4/The Slicing Type/String Slices/example_3/src/main.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let s = String::from("hello");
+
+    let len = s.len();
+
+    let slice = &s[3..len];
+    let slice = &s[3..];
+}
---
+++ Chapter 4/The Slicing Type/String Slices/example_4/src/main.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let s = String::from("hello");
+
+    let len = s.len();
+
+    let slice = &s[0..len];
+    let slice = &s[..];
+}

9a4c93dd0adff50f988f55b6ddc048a3e743b022
jminjares4 <minjares.jesus.ths@gmail.com>
2023-07-15 17:46:07 -0600

    Added Storing Keys with Associated Values in Hash Maps

---
+++ Chapter 8/Storing Keys with Associated Values in Hash Maps/Creating a New Hash Map/example_1/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    use std::collect::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+}
